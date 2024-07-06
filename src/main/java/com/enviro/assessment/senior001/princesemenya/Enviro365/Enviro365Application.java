ca90ed4d3e640aec7e17b4a03a87d449dfccd929
Prince <semenya143@gmail.com>
2024-07-06 10:36:06 +0200

    Initial commit

---
+++ src/main/java/com/enviro/assessment/senior001/princesemenya/Enviro365/Enviro365Application.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.senior001.princesemenya.Enviro365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Enviro365Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/senior001/princesemenya/Enviro365/Enviro365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.senior001.princesemenya.Enviro365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Enviro365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
